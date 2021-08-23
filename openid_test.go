41f44ad0158a93c28e18bc100d7e9fd546375a2b
周帅明 <smzhou@corp.netease.com>
2021-08-23 13:36:12 +0800

    clean useless code

--- openid_test.go
+++ openid_test.go
@@ -1,20 +1,20 @@
 package openid
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
 
 var realm = "https://localhost"
-var opEndpoint = "https://login.example.com/openid"
-var callbackPrefix = "/openid/verify"
+
+// var opEndpoint = "https://login.example.com/openid"
+// var callbackPrefix = "/openid/verify"
 
 // How to test openid without a openid provider ?
 // The fake login.example.com/openid is not exist.
 func Test_New_0(t *testing.T) {
 	o := New(realm)
-	if "*openid.OpenID" != fmt.Sprintf("%s", reflect.TypeOf(o)) {
+	if reflect.TypeOf(o).String() != "*openid.OpenID" {
 		t.Errorf("New return type error")
 	}
 }
