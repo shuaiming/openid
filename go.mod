4fdd319c5ee46bda3921ed28845ee7a6a9bac664
周帅明 <smzhou@corp.netease.com>
2022-08-22 16:23:57 +0800

    clear id module

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/shuaiming/openid
 
 go 1.18
-
-require github.com/shuaiming/sessions v0.0.4
--- vendor/github.com/shuaiming/sessions/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2021 shuaimingzhou(at)gmail(dot)com.
-// All rights reserved. License that can be found in the LICENSE file.
-
-/* http sessions and stores */
-package sessions
